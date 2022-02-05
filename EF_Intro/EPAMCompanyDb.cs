7131bb5095147c0cea8323cdd1f4b1d95b0891cd
vladtymo <tymo.vlad@gmail.com>
2022-02-05 13:39:18 +0200

    Add project files.

---
+++ EF_Intro/EPAMCompanyDb.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Intro
+{
+    public class EPAMCompanyDb : DbContext // NuGet - EntityFrameworkCore (.SqlServer)
+    {
+        public EPAMCompanyDb()
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"data source=(LocalDb)\MSSQLLocalDB;initial catalog=EPUM_Db;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+        }
+
+        // Collections (tables in db)
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+
+    public class Department
+    {
+        [Key]   // set primary key
+        public int Number { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(30)]
+        public string Phone { get; set; }
+
+        // Navigation Properties
+        public ICollection<Worker> Workers { get; set; }
+    }
+
+    // Entities
+    public class Worker
+    {
+        // Properties (columns in db)
+
+        // Primary Key: Id/ID/id EntityName+Id (WorkerId)
+        public int Id { get; set; }
+        [Required]          // set not null
+        [MaxLength(200)]    // set max length (NVarChar(200))
+        public string Name { get; set; }
+        [Required, MaxLength(200)]
+        public string Surname { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public decimal Salary { get; set; }
+
+        // Navigation Properties
+        [Required]
+        public Department Department { get; set; }
+    }
+}
---
+++ EF_Intro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace EF_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EPAMCompanyDb db = new EPAMCompanyDb();
+
+            //db.Departments.Add(new Department()
+            //{
+            //    Name = "Security Programming",
+            //    Phone = "3455-223-44"
+            //});
+            //db.SaveChanges();
+
+            var result = db.Workers.Where(w => w.Salary > 1000);
+
+            foreach (var item in db.Departments)
+            {
+                Console.WriteLine($"Department #{item.Number} {item.Name} {item.Phone}");
+            }
+        }
+    }
+}
